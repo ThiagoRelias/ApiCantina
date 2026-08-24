03fd16504c9f29041d6071fb7adb2bd3ef8e8778
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-08-24 11:07:10 -0300

    Adicionando os aplications

---
+++ src/main/java/ApiCantina/ApiCantinaApplication.java
@@ -0,0 +1,13 @@
+package ApiCantina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCantinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCantinaApplication.class, args);
+	}
+
+}
